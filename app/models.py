0f85d23bce44eefca89e72ddaf3fe361e5f9c210
webdeveloper510 <developerweb6@gmail.com>
2022-02-10 17:59:09 +0530

    09/02/2022

--- app/models.py
+++ app/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import PermissionsMixin
 from django.utils.translation import gettext_lazy as _
 from .manager import CustomUserManager
 from django.core.validators import validate_comma_separated_integer_list
+from phone_field import PhoneField
 
 # Create your models here.
 class user(AbstractBaseUser,PermissionsMixin):
@@ -14,7 +15,7 @@ class user(AbstractBaseUser,PermissionsMixin):
     first_name=models.CharField(max_length=200,null=True)
     last_name=models.CharField(max_length=200,null=True)
     date_of_birth=models.DateField(null=True)
-    phone_number=models.IntegerField(null=True,default=0)
+    phone_number=PhoneField(null=True,default=0)
     #address=models.CharField(max_length=100,null=True)
     is_staff 	= models.BooleanField(default=False, help_text='Designates whether the user can log into this admin site.')
     is_active 	= models.BooleanField(default=True,
