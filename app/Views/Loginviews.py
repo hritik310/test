18bf1c8e815a80358cac3ce8b0ab72c4b451aadc
webdeveloper510 <developerweb6@gmail.com>
2022-03-02 11:37:37 +0530

    push

--- app/Views/Loginviews.py
+++ app/Views/Loginviews.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from app.forms.update import userform
 from app.models import *
 from django.contrib.auth import authenticate, login,logout
 from django.shortcuts import redirect
@@ -9,7 +10,7 @@ from django.contrib import auth
 from django.views.decorators.cache import cache_control
 from django.contrib.auth.decorators import login_required
 from app.helper import *
-
+from app.forms.update import *
 
 
 
@@ -64,15 +65,31 @@ def update(request,id):
       owner_id=show[0]
   else:
     owner_id=0   
-  if request.method == "POST":
-
-        custom.username = request.POST.get('username')
-        custom.email = request.POST.get('email')
-        custom.phone_number= request.POST.get('phone_number')
-        custom.save()
-        return render(request,"login/update.html")
+  context = {
+        "form": userform()
+    }
   
-  return render (request,"login/update.html",{'show':owner_id,'customer':custom})
+  accountform =  userform(instance=custom)
+  print("request",request.user)
+  print("accountform",accountform)
+  if request.method == "POST":
+    accountform = userform(request.POST,instance=custom)
+    if accountform.is_valid():
+        print("True")
+        accountform.save()
+
+        # custom.username = request.POST.get('username')
+        # custom.email = request.POST.get('email')
+        # custom.phone_number= request.POST.get('phone_number')
+        # custom.save()
+          
+    
+    else:      
+      print("false")
+      return render(request,"login/update.html",{'form':accountform,"context":context})
+
+  
+  return render (request,"login/update.html",{'show':owner_id,'customer':custom,'form':accountform})
 
 
 
---
+++ app/forms/update.py
@@ -0,0 +1,58 @@
+from django.forms import IntegerField, ModelForm
+from django import forms
+from datetime import datetime
+
+from flask import request
+from app.models import *
+from django.contrib.auth.password_validation import validate_password
+from django.core import validators
+from datetime import date
+from django.core.validators import RegexValidator
+from django.core.exceptions import ValidationError
+
+
+
+class userform(ModelForm):
+    email = forms.CharField(required = True,widget=forms.TextInput(
+        attrs={
+        'class':'form-control',
+        'placeholder':'Enter your Email address'
+        }
+    ))
+    username = forms.CharField(label="Username",required = True,widget=forms.TextInput(
+        attrs={
+        'class':'form-control',
+        'placeholder':'Enter your name',
+        
+        
+        }
+
+    ))
+
+    phone_number=forms.IntegerField(label="Phone Number",required = True,widget=forms.TextInput(
+        attrs={
+        'max_length':10,
+        'class':'form-control',
+        'placeholder':'Enter Phone number',
+        'type':'number'
+        
+        }
+
+    ))
+
+    class Meta:
+        model = user
+        fields = ["username","email","phone_number"]
+
+
+    
+    def clean(self):
+          cleaned_data=super(userform, self).clean()   
+      
+          phone_number= cleaned_data.get('phone_number')
+          ph_num=str(phone_number)
+          print(ph_num)#dflklg
+          if len(ph_num)>15 or len(ph_num)<10:
+            print("dfs")
+            self.add_error("phone_number", forms.ValidationError("Phone number must be between 10 and 15 digit")
+            )    
