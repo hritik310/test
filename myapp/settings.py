add7ea6d011e3354ff774668640667cf6761b73a
webdeveloper510 <developerweb6@gmail.com>
2022-07-28 09:42:39 +0530

    again

--- myapp/settings.py
+++ myapp/settings.py
@@ -37,7 +37,7 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
         messages.DEBUG: 'alert-secondary',
         messages.INFO: 'alert-info',
-        messages.SUCCESS: 'alert-success',
+        messages.SUCCESS: 'alert-success',      
         messages.WARNING: 'alert-warning',
         messages.ERROR: 'alert-danger',
  }
